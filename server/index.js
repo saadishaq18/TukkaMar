c838e52d47aaa7c1c021e8bcddab20562ff36708
saadishaq18 <saadishaq97@gmail.com>
2025-06-21 13:47:35 +0500

    added socket.io logics

--- server/index.js
+++ server/index.js
@@ -19,11 +19,45 @@ app.get('/', (req, res) => {
   res.send('Server running!');
 });
 
+const users = {}
 // Socket.IO logic
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
 
+  socket.on('join_room', ({roomId, nickname})=>{
+    users[socket.id] = {nickname, roomId}
+    socket.join(roomId)
+
+    const roomUsers = Object.entries(users)
+        .filter(([id, user])=> user.roomId === roomId)
+        .map(([id, user])=>({id, nickname: user.nickname}))
+
+    io.to(roomId).emit('room_user', roomUsers)
+  })
+
+  socket.on('leave_room', ({roomId})=>{
+    socket.leave(roomId)
+    delete users[socket.id]
+
+    const roomUsers = Object.entries(users)
+    .filter(([id, user])=>user.roomId === roomId)
+    .map(([id, user])=> ({id, nickname: user.nickname}))
+
+    io.to(roomId).emit('room_users', roomUsers)
+  })
+
   socket.on('disconnect', () => {
+    const user = users[socket.id]
+    if(user){
+        const roomId = user.roomId
+        delete users[socket.id]
+
+        const roomUsers = Object.entries(users)
+            .filter(([id,u])=>u.roomId === roomId)
+            .map(([id,u])=>({id, nickname: u.nickname}))
+
+        io.to(roomId).emit('room_users', roomUsers)
+    }
     console.log('User disconnected:', socket.id);
   });
 });
